098b7c329a2d1a599013a54594559721ca3c1eb2
Gustavo Santana <gustavo.santana@becareapp.io>
2025-01-26 09:33:51 -0300

    refactor: enhance DeleteUserController by utilizing helper functions for response handling

--- src/controllers/delete-user/delete-user.ts
+++ src/controllers/delete-user/delete-user.ts
@@ -1,32 +1,24 @@
 import { User } from "../../models/user";
+import { badRequest, ok, serverError } from "../helpers";
 import { HttpRequest, HttpResponse, IController } from "../protocols";
 import { IDeleteUserRepository } from "./procotols";
 
 export class DeleteUserController implements IController {
   constructor(private readonly deleteUserRepository: IDeleteUserRepository) {}
   
-  async handle(HttpRequest: HttpRequest<{ id: string }>): Promise<HttpResponse<User>> {
+  async handle(HttpRequest: HttpRequest<{ id: string }>): Promise<HttpResponse<User | string>> {
     try {
        const id = HttpRequest?.params?.id;
 
        if (!id) {
-         return {
-           statusCode: 400,
-           body: 'Missing user Id'
-         };
+         return badRequest('Missing user id');
        }
 
       const user = await this.deleteUserRepository.deleteUser(id);
 
-       return {
-         statusCode: 200,
-         body: user
-       };
+       return ok(user);
     } catch {
-      return {
-        statusCode: 500,
-        body: 'Internal server error'
-      };
+      return serverError();
     }
   }
 }
